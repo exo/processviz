6792b84e21a6b662671ef3159ac191fb48353861
Christian L. Jacobsen <cljacobsen@gmail.com>
2010-08-13 17:12:19 +0100

    Implement delete (from context menu)
    Delete is currently implemented by calling remove_process on the network.
    Remove process has been augmented to also delete the relevant channels
    connected to that process. I think that is the correct place for that
    responsibility, though in can potentially see cases where it might not be. Say
    that I want to implement some code to replace one process (object) with
    another, i might want to remove a process from the network w/o removing the
    channels... discuss in no more than four A4 pages.

--- canvas/model/network.py
+++ canvas/model/network.py
@@ -12,6 +12,11 @@ class Network (object):
         self._processes.append(process)
 
     def remove_process (self, process):
+        chan_ends = process.get_input_chans() + process.get_output_chans()
+        # Make a copy of self._channels, as we are modifying it in the for loop
+        for chan in self._channels[:]:
+            if chan.src in chan_ends or chan.dest in chan_ends:
+                self.remove_channel(chan)
         self._processes.remove(process)
 
     def get_channels (self):
@@ -21,6 +26,9 @@ class Network (object):
 
     def add_channel (self, channel):
         self._channels.append(channel)
+
+    def remove_channel (self, channel):
+        self._channels.remove(channel)
 
     def structure (self):
         return dict(_type='Network', channels=self._channels, processes=[n.structure() for n in self._processes])
